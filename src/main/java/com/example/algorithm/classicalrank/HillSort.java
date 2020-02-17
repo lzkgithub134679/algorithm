2be63eff40a157822f9ea6d53b537432c15a0729
lizhikang <lizhikang@youngyedu.com>
2020-02-17 15:22:25 +0800

    增加img文件

---
+++ src/main/java/com/example/algorithm/classicalrank/HillSort.java
@@ -0,0 +1,10 @@
+package com.example.algorithm.classicalrank;
+
+/**https://sort.hust.cc/4.shellsort
+ *
+ * @Description : 希尔排序
+ * @Author : lizhikang@youngyedu.com, 2020/1/21 10:16
+ * @Modified : lizhikang@youngyedu.com, 2020/1/21
+ */
+public class HillSort {
+}
---
+++ src/main/java/com/example/algorithm/classicalrank/InsertSort.java
@@ -0,0 +1,38 @@
+package com.example.algorithm.classicalrank;
+
+import java.util.Arrays;
+
+/** https://sort.hust.cc/3.insertionsort
+ *
+ * @Description : 插入排序
+ * 插入排序的代码实现虽然没有冒泡排序和选择排序那么简单粗暴，但它的原理应该是最容易理解的了，
+ * 因为只要打过扑克牌的人都应该能够秒懂。插入排序是一种最简单直观的排序算法，它的工作原理是通过构建有序序列，
+ * 对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+ * 插入排序和冒泡排序一样，也有一种优化算法，叫做拆半插入。
+ * @Author : lizhikang@youngyedu.com, 2020/1/21 9:35
+ * @Modified : lizhikang@youngyedu.com, 2020/1/21
+ */
+public class InsertSort {
+
+    public static int [] insertSort(int [] arr){
+        int num = 0;
+        for(int i = 1;i<arr.length;i++){
+            int temp = arr[i];
+            int leftTemp = i-1;
+            while(leftTemp>=0 && arr[leftTemp]>temp){
+                arr[leftTemp+1] = arr[leftTemp];
+                leftTemp--;
+                num++;
+            }
+            num++;
+            arr[leftTemp+1]  = temp;
+        }
+        System.out.println("执行循环次数: "+num);
+        return arr;
+    }
+    public static void main(String [] args){
+        int [] arr = new int[]{34,324,23,42,34,1,23,12,3};
+        insertSort(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
